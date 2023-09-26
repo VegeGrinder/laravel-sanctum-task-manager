eae98e26b96f1bb9833c899286c5bd2bacb128a1
Shang Shein Goh <gohsshein@gmail.com>
2023-09-26 17:29:59 +0800

    Add Command to delete archived Task (scheduler)

---
+++ app/Console/Commands/DeleteArchivedTaskCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Task;
+use Illuminate\Console\Command;
+
+class DeleteArchivedTaskCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-archived-task-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete archived Tasks older than 1 week.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $count = Task::where('is_archived', true)->where('created_at', '<=', now()->subDays(7))->count();
+        Task::where('is_archived', true)->where('created_at', '<=', now()->subDays(7))->delete();
+
+        $this->info("Command finished, $count Task(s) deleted.");
+    }
+}
